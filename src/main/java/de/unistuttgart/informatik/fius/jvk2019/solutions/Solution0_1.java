f40c16a188daf7653a088dcd55a94c9e92efa03b
ModellbahnFreak <freak.mdellbahn@gmail.com>
2019-10-09 17:17:11 +0200

    Added Gravity to task 0_1

--- src/main/java/de/unistuttgart/informatik/fius/jvk2019/solutions/Solution0_1.java
+++ src/main/java/de/unistuttgart/informatik/fius/jvk2019/solutions/Solution0_1.java
@@ -10,6 +10,7 @@
 package de.unistuttgart.informatik.fius.jvk2019.solutions;
 
 import de.unistuttgart.informatik.fius.icge.simulation.Simulation;
+import de.unistuttgart.informatik.fius.jvk2019.provided.GravitySimulation;
 import de.unistuttgart.informatik.fius.jvk2019.provided.entity.Neo;
 import de.unistuttgart.informatik.fius.jvk2019.tasks.Task0;
 
@@ -20,24 +21,33 @@ import de.unistuttgart.informatik.fius.jvk2019.tasks.Task0;
  * @author Fabian Bühler
  */
 public class Solution0_1 extends Task0 {
-    
+
+    private GravitySimulation grav;
+
     @Override
     public void prepare(Simulation sim) {
         super.prepare(sim);
         this.player = new Neo();
         this.spawnEntity(this.player, 0, 0);
+        this.grav = new GravitySimulation(sim);
     }
     
     @Override
     public void solve() {
         // simple solve method directly calling the entity methods
         this.player.move();
+        this.grav.simulate();
         this.player.move();
+        this.grav.simulate();
         this.player.turnClockWise();
+        this.grav.simulate();
         this.player.move();
+        this.grav.simulate();
         this.player.move();
+        this.grav.simulate();
         if (this.player.canMove()) {
             this.player.move();
+            this.grav.simulate();
         }
     }
     
