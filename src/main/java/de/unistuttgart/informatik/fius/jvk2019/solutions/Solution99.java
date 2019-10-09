8bd778681bdbc3f4ed0b37326c281a8b05369222
ModellbahnFreak <freak.mdellbahn@gmail.com>
2019-10-09 17:18:14 +0200

    Second part solution as added in Task 99

--- src/main/java/de/unistuttgart/informatik/fius/jvk2019/solutions/Solution99.java
+++ src/main/java/de/unistuttgart/informatik/fius/jvk2019/solutions/Solution99.java
@@ -7,14 +7,19 @@ import de.unistuttgart.informatik.fius.jvk2019.provided.entity.MyNeo;
 import de.unistuttgart.informatik.fius.jvk2019.provided.entity.Portal;
 import de.unistuttgart.informatik.fius.jvk2019.tasks.Task99;
 
+import java.lang.reflect.Field;
 import java.util.List;
+import java.util.Set;
+import java.util.concurrent.CompletableFuture;
+import java.util.function.Function;
 
 public class Solution99 extends Task99 {
+
+    private int counter = 0;
 
     @Override
     public void prepare(Simulation sim) {
         super.prepare(sim);
-
     }
 
     @Override
@@ -53,5 +58,18 @@ public class Solution99 extends Task99 {
         /*while (player.canMove()) {
             player.move();
         }*/
+
+        generateSecondPart();
+
+        /*while (!player.isOnPhoneBooth()) {
+            while (player.canMove()) {
+                player.move();
+            }
+            player.turnClockWise();
+        }*/
+
+        System.out.println("Run: " + this.counter);
+        counter++;
+        //new Exception("Stacktrace").printStackTrace();
     }
 }
