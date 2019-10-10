8193d1759671585a0c6364f136c93d70734010d4
nk-coding <53957498+nk-coding@users.noreply.github.com>
2019-10-10 16:46:58 +0200

    solution 4_2 and MySmartNeo imp solution for exercise 1

--- src/main/java/de/unistuttgart/informatik/fius/jvk2019/solutions/Solution4_2.java
+++ src/main/java/de/unistuttgart/informatik/fius/jvk2019/solutions/Solution4_2.java
@@ -1,7 +1,11 @@
 package de.unistuttgart.informatik.fius.jvk2019.solutions;
 
 import de.unistuttgart.informatik.fius.icge.simulation.Simulation;
+import de.unistuttgart.informatik.fius.jvk2019.provided.entity.MySmartNeo;
+import de.unistuttgart.informatik.fius.jvk2019.provided.entity.SmartNeo;
 import de.unistuttgart.informatik.fius.jvk2019.tasks.Task4_2;
+
+import java.util.concurrent.CompletableFuture;
 
 public class Solution4_2 extends Task4_2 {
     @Override
@@ -10,10 +14,15 @@ public class Solution4_2 extends Task4_2 {
 
         // TODO solve 4.1 c) here
         // Use the 'this.neo' attribute to solve this task
+        this.neo = new MySmartNeo();
+        spawnEntity(neo, 0, 0);
     }
 
     @Override
     public void solve() {
         // TODO solve 4.1 c) here
+        while (!neo.isOnPhoneBooth()) {
+            neo.moveSmart();
+        }
     }
 }
