3169d466391468f2423d865f30b36f33a3e83ec3
ModellbahnFreak <freak.mdellbahn@gmail.com>
2019-10-10 18:00:56 +0200

    Solved task 3_3b

--- src/main/java/de/unistuttgart/informatik/fius/jvk2019/solutions/Solution3_3b.java
+++ src/main/java/de/unistuttgart/informatik/fius/jvk2019/solutions/Solution3_3b.java
@@ -14,13 +14,26 @@ import de.unistuttgart.informatik.fius.jvk2019.provided.entity.Pill;
 import de.unistuttgart.informatik.fius.jvk2019.tasks.Task3_3b;
 
 /**
- * TODO: Solution for ex 3 (b) from worksheet 3
+ *
  * @author Sebastian "Zottel92" Grund
  */
 public class Solution3_3b extends Task3_3b {
     
     @Override
     public void solve() {
+        while (!neo.isOnPhoneBooth()) {
+            while (neo.peakPill() == null && !neo.isOnPhoneBooth()) {
+                neo.move();
+            }
+            if (!neo.isOnPhoneBooth()) {
+                if (neo.peakPill().getColor() == Color.RED) {
+                    neo.turnClockWise();
+                } else {
+                    neo.turnCounterClockwise();
+                }
+                neo.collectOnField();
+            }
+        }
     }
     
 }
