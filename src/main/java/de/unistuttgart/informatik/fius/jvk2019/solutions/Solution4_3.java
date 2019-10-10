130905f36f90c669d855bc667454de01f01a9f0e
nk-coding <53957498+nk-coding@users.noreply.github.com>
2019-10-10 16:35:16 +0200

    solution 4_3

--- src/main/java/de/unistuttgart/informatik/fius/jvk2019/solutions/Solution4_3.java
+++ src/main/java/de/unistuttgart/informatik/fius/jvk2019/solutions/Solution4_3.java
@@ -10,8 +10,13 @@
 package de.unistuttgart.informatik.fius.jvk2019.solutions;
 
 import de.unistuttgart.informatik.fius.icge.simulation.Simulation;
+import de.unistuttgart.informatik.fius.jvk2019.provided.Color;
 import de.unistuttgart.informatik.fius.jvk2019.provided.entity.MyNeo;
+import de.unistuttgart.informatik.fius.jvk2019.provided.entity.Pill;
 import de.unistuttgart.informatik.fius.jvk2019.tasks.Task4_3;
+
+import java.util.ArrayList;
+import java.util.Collections;
 
 
 /**
@@ -36,6 +41,38 @@ public class Solution4_3 extends Task4_3 {
         // If he found a red pill he needs to reverse the order of the coin stacks.
         // When he finishes this task the phone will ring and he can escape the 
         // matrix by using it (beeing on the same tile).
+        MyNeo neo = this.player;
+        //check pill
+        Pill pill = neo.peakPill();
+        neo.collectPill();
+        boolean reversed = pill.getColor() == Color.BLUE;
+        ArrayList<Integer> amountCoins = new ArrayList<>();
+        while (neo.canMove()) {
+            neo.move();
+            int amount = 0;
+            while (neo.canCollectCoin()) {
+                amount++;
+                neo.collectCoin();
+            }
+            amountCoins.add(amount);
+        }
+        //sort with the reversed order
+        amountCoins.sort(Integer::compareTo);
+        if (reversed) Collections.reverse(amountCoins);
+        //turn neo
+        neo.turnAround();
+        for (int amount : amountCoins) {
+            for (int x = 0; x < amount; x++) {
+                neo.dropCoin();
+            }
+            neo.move();
+        }
+        neo.turnAround();
+        while (neo.canMove()) {
+            neo.move();
+        }
+
+        //System.out.println("return");
     }
     
 }
